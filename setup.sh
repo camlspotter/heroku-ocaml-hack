4eb39e1fc1aabb1db164b21bb534bf49a00fd256
Jun Furuse <jun.furuse@gmail.com>
2013-06-09 15:47:55 +0000

    dir

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo BUILDDIR=$BUILDDIR
+echo BUILD_DIR=$BUILD_DIR
 echo PATH=$PATH
 echo LD_LIBRARY_PATH=$LD_LIBRARY_PATH
 
@@ -10,15 +10,15 @@ PREFIX=/app/vendor/opam-lib/system/bin omake
 mkdir -p target/bin/
 cp main target/bin/main
 
-mkdir -p /app/runtime
-mkdir -p /app/runtime/lib
+mkdir -p runtime
+mkdir -p runtime/lib
 cp -a /app/vendor/opam-lib runtime/opam
 cp -a /app/vendor/pcre/lib/* runtime/lib
 cp -a /app/vendor/gdbm/lib/* runtime/lib
 
 # deploy done
 
-export LD_LIBRARY_PATH="$BUILDDIR/runtime/lib:$LD_LIBRARY_PATH"
-export PATH="$BUILDDIR/runtime/opam/system/bin:$BUILDDIR/runtime/bin:$PATH"
-export CAML_LD_LIBRARY_PATH=$BUILDDIR/runtime/opam/system/lib/stublibs
+export LD_LIBRARY_PATH="$BUILD_DIR/runtime/lib:$LD_LIBRARY_PATH"
+export PATH="$BUILD_DIR/runtime/opam/system/bin:$BUILDDIR/runtime/bin:$PATH"
+export CAML_LD_LIBRARY_PATH=$BUILD_DIR/runtime/opam/system/lib/stublibs
 
