9cf2ab00bc31d4392500273657ee6083112cc92a
Jun Furuse <jun.furuse@gmail.com>
2014-01-25 14:03:53 +0000

    simplified setup.sh

--- setup.sh
+++ setup.sh
@@ -30,77 +30,77 @@ setup /app/vendor/gdbm/ http://49.212.130.159:5963/heroku/gdbm-1.tgz
 export PATH="/app/vendor/gdbm/bin:$PATH"
 export LD_LIBRARY_PATH=/app/vendor/gdbm/lib:$LD_LIBRARY_PATH
 
-setup /app http://49.212.130.159:5963/heroku/opam-lib.tgz
-
-# # . /app/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
+# setup /app http://49.212.130.159:5963/heroku/opam-lib.tgz
 # 
-# ################################################# heroku special repo
+# # # . /app/.opam/opam-init/init.sh > /dev/null 2> /dev/null || true
+# # 
+# # ################################################# heroku special repo
+# # 
+# # git clone https://github.com/camlspotter/opam-repository-heroku.git
+# # mv opam-repository-heroku /app/opam-repository-heroku
+# # # this is to update only heroku
+# # if [ -d /app/.opam/heroku ]; then
+# #   opam repo remove heroku
+# # fi
+# # opam repo add heroku /app/opam-repository-heroku
 # 
-# git clone https://github.com/camlspotter/opam-repository-heroku.git
-# mv opam-repository-heroku /app/opam-repository-heroku
-# # this is to update only heroku
-# if [ -d /app/.opam/heroku ]; then
-#   opam repo remove heroku
-# fi
-# opam repo add heroku /app/opam-repository-heroku
-
-################################################# build opam
-# It fails...
-# setup /app http://49.212.130.159:5963/heroku/opam.1.0.1.tgz
-# (cd /app/opam; ./configure --prefix /app/.share/prefix; make; make install)
-
-# # opam switch 4.00.1
-# # /bin/rm -rf /app/.opam/4.00.1/build
+# ################################################# build opam
+# # It fails...
+# # setup /app http://49.212.130.159:5963/heroku/opam.1.0.1.tgz
+# # (cd /app/opam; ./configure --prefix /app/.share/prefix; make; make install)
 # 
-# # opam default has strange behaviour
+# # # opam switch 4.00.1
+# # # /bin/rm -rf /app/.opam/4.00.1/build
+# # 
+# # # opam default has strange behaviour
+# # 
+# # opam switch list
+# # # opam switch 4.00.1+custom
+# # # opam switch remove 4.00.1
 # 
-# opam switch list
-# # opam switch 4.00.1+custom
-# # opam switch remove 4.00.1
-
-#################################################### build copy image
-
-# /bin/rm -rf /app/.opam/log/*
-# /bin/rm -rf /app/.opam/4.00.1+custom/build/*
-
-eval `opam config env`
-opam install -y omake
-opam install -y spotlib
-# opam install treeprint orakuda CamlGI meta_conv pa_ovisitor
-# tar zcvf opam-lib.tgz -C /app .opam
-
-# ########################################## copy which I want to use later
+# #################################################### build copy image
 # 
-# mkdir -p vendor
-# cp -a /app/vendor/ocaml vendor/
-# 
-# ################################################## copy server
+# # /bin/rm -rf /app/.opam/log/*
+# # /bin/rm -rf /app/.opam/4.00.1+custom/build/*
 # 
 # eval `opam config env`
 # opam install -y omake
 # opam install -y spotlib
-mkdir -p $HOME/.share/prefix
-export PREFIX=$HOME/.share/prefix
-omake
-mkdir -p target/bin/
-cp main target/bin/main
-
-# opam install -y dbm
-# opam install -y eliom
-
-# OPAM repo tweak
-# opam repo add opam  http://opam.ocamlpro.com
-# git clone https://github.com/camlspotter/opam-repository-heroku.git
-# cp -a opam-repository-heroku /app/opam-repository-heroku
-# opam repo add heroku /app/opam-repository-heroku
-# opam repo remove default
-# opam update
-
-# opam switch 4.00.1+custom
-# opam switch remove -y 4.00.1
-# opam switch remove -y system
-
-# opam install -y dbm
-# opam install -y eliom
-
-./build-ocamloscope.sh
+# # opam install treeprint orakuda CamlGI meta_conv pa_ovisitor
+# # tar zcvf opam-lib.tgz -C /app .opam
+# 
+# # ########################################## copy which I want to use later
+# # 
+# # mkdir -p vendor
+# # cp -a /app/vendor/ocaml vendor/
+# # 
+# # ################################################## copy server
+# # 
+# # eval `opam config env`
+# # opam install -y omake
+# # opam install -y spotlib
+# mkdir -p $HOME/.share/prefix
+# export PREFIX=$HOME/.share/prefix
+# omake
+# mkdir -p target/bin/
+# cp main target/bin/main
+# 
+# # opam install -y dbm
+# # opam install -y eliom
+# 
+# # OPAM repo tweak
+# # opam repo add opam  http://opam.ocamlpro.com
+# # git clone https://github.com/camlspotter/opam-repository-heroku.git
+# # cp -a opam-repository-heroku /app/opam-repository-heroku
+# # opam repo add heroku /app/opam-repository-heroku
+# # opam repo remove default
+# # opam update
+# 
+# # opam switch 4.00.1+custom
+# # opam switch remove -y 4.00.1
+# # opam switch remove -y system
+# 
+# # opam install -y dbm
+# # opam install -y eliom
+# 
+# ./build-ocamloscope.sh
