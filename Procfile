ca0a8489db291eb42fe4deb767d0feb91321fe18
Jun Furuse <jun.furuse@gmail.com>
2014-02-02 12:48:47 +0000

    test run

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./target/bin/main $PORT
+web: ./run.sh $PORT
---
+++ run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+cd src
+
+export LD_LIBRARY_PATH=/app/vendor/gdbm/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/app/vendor/pcre/lib:$LD_LIBRARY_PATH
+export PATH=/app/.opam/system/bin:$PATH
+
+PORT=$1
+sed -e s/@PORT@/$PORT/g oco.conf.in > oco.conf
+
+mkdir -p /app/tmp/var/log/oco
+mkdir -p /app/tmp/var/run
+mkdir -p /app/tmp/var/data/oco/ocsipersist
+
+/app/.opam/system/bin/ocsigenserver.opt -c oco.conf
+
